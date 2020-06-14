690f834dd37744dc6c121a24d9710af54ffaf32f
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-06-14 21:44:30 +0900

    ユーザ情報、　tokenの取得

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,13 +1,39 @@
 import axios from 'axios';
-
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
 import { setAlert } from './alert';
+import setAuthToken from '../utils/setAuthToken';
+
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+} from './types';
+
+//Load User
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get('/api/auth');
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
 
 //ユーザ登録
 export const register = ({ name, email, password }) => async (dispatch) => {
   const config = {
     headers: {
-      'COntent-Type': 'application/json',
+      'Content-Type': 'application/json',
     },
   };
 
