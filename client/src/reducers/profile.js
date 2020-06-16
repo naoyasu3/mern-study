182e5c7ee38a4289f09da4fee273565e56e3a438
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-06-16 12:07:05 +0900

    ADD redux, get all profile, get by id profile, get github repos

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,8 +1,10 @@
 import {
   GET_PROFILE,
+  GET_PROFILES
   UPDATE_PROFILE,
   PROFILE_ERROR,
   CLEAR_PROFILE,
+  GET_REPOS,
 } from '../actions/types';
 
 const initialState = {
@@ -24,6 +26,12 @@ export default function (state = initialState, action) {
         profile: payload,
         loading: false,
       };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false,
+      }
     case PROFILE_ERROR:
       return {
         ...state,
@@ -37,6 +45,12 @@ export default function (state = initialState, action) {
         profile: null,
         repos: [],
       };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
+        loading: false
+      }
     default:
       return state;
   }
